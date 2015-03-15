dbb63443f54e37431903aca6ad665b72f696f857
Alvaro Masitsa <alvaromasitsa104@gmail.com>
2015-03-15 21:33:01 +0300

    Default Slider

--- application/modules/site/views/home/slider.php
+++ application/modules/site/views/home/slider.php
@@ -1,4 +1,4 @@
- <?php
+<?php
   	if($banners->num_rows() > 0)
 	{
 		$banner_no = $banners->num_rows();
@@ -56,51 +56,63 @@
 		}
 		?>
   		</div>
-  <?php
-	if ($banner_no > 0)
-	{
-		?>
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-<?php
-    }
+		<?php
+        if ($banner_no > 0)
+        {
+        ?>
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+		<?php
+        }
 	}
-  ?>
-    <!--<div class="item active">
-    	<img src="http://placehold.it/1200x400/16a085/ffffff&text=About Us">
-      <div class="carousel-caption">
-        <h3>Headline</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem
-                    ipsum dolor sit amet, consetetur sadipscing elitr.</p>
-      </div>
-    </div>
-    <div class="item">
-            <img src="http://placehold.it/1200x400/e67e22/ffffff&text=Projects">
-      <div class="carousel-caption">
-        <h3>Headline</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem
-                    ipsum dolor sit amet, consetetur sadipscing elitr.</p>
-      </div>
-    </div>
-    <div class="item">
-            <img src="http://placehold.it/1200x400/8e44ad/ffffff&text=Services">
-      <div class="carousel-caption">
-        <h3>Headline</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem
-                    ipsum dolor sit amet, consetetur sadipscing elitr.</p>
-      </div>
-    </div>-->
-
+	
+	else
+	{
+    ?>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+        	<li data-target="#carousel-example-generic" data-slide-to="1" class="active"></li>
+        	<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        	<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        </ol>
+        
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="http://placehold.it/1200x400/55a4da/ffffff&text=In Store Look">
+                <div class="carousel-caption">
+                    <p>An online ecommerce platform connecting businesses & customers.</p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://placehold.it/1200x400/e67e22/ffffff&text=Business">
+                <div class="carousel-caption">
+                    <p>Sign up to create an online presence for uploading & selling products to customers.</p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://placehold.it/1200x400/8e44ad/ffffff&text=Customer">
+                <div class="carousel-caption">
+                    <p>Browse & buy products from your local surburb.</p>
+                </div>
+            </div>
+        </div>
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+<?php
+	}
+?>
   <!-- Controls -->
